46f59bf2707c824c50a3417c5204561f97a97130
WallRunner <47185839+JAcamp1@users.noreply.github.com>
2024-09-28 00:40:55 -0400

    Added backend files

---
+++ Backend/sideEffects.py
@@ -0,0 +1,15 @@
+#python-3.10.12
+
+import json
+import pandas as pd
+
+with open("medicineLookup.json", "r") as f:
+    lookup = json.load(f)
+
+sideEffects = pd.read_csv("RawData/ChSe-Decagon_monopharmacy.csv")
+
+def getMedicationCode(name : str):
+    return lookup.get(name)
+    
+def getSideEffects(code : str):
+    print(sideEffects.loc[sideEffects["# STITCH"] == code]["Side Effect Name"])
