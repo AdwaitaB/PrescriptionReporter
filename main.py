9cfd131d39dc67f79ea3e0e368687e9868162907
WallRunner <47185839+JAcamp1@users.noreply.github.com>
2024-09-28 23:13:24 -0400

    Adapted Files for Flask Structure

--- main.py
+++ main.py
@@ -1,10 +1,14 @@
-from flask import Flask, abort
+from flask import Flask, abort, render_template
 
 app = Flask(__name__)
 
 @app.route("/")
 def index():
     abort(404)
+
+@app.route("/prescriber/dashboard")
+def prescriberDash():
+    return render_template("prescriber.html")
 
 if __name__ == "__main__":
     pass
